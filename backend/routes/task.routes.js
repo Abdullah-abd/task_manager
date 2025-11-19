b4c15b43f624e359f02d4e74f974aa66e0f94998
Abdullah <abdullah262001@gmail.com>
2025-11-19 18:53:29 +0530

    feat (get-task):get single task implemented

--- backend/routes/task.routes.js
+++ backend/routes/task.routes.js
@@ -1,9 +1,14 @@
 // routes/task.routes.js
 import express from "express";
-import { createTask, getAllTasks } from "../controllers/task.controller.js";
+import {
+  createTask,
+  getAllTasks,
+  getTask,
+} from "../controllers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/", createTask);
 router.get("/", getAllTasks);
+router.get("/:id", getTask);
 export default router;
